41eda0c77250042ce35aa03f168ebe184932f22d
EllenWhelan <whelane5@tcd.ie>
2020-10-06 19:36:13 +0100

    added old excercises

---
+++ Ex01.hs
@@ -0,0 +1,35 @@
+-- Name: Ellen Whelan,  Username: whelane5
+module Ex01 where
+import Data.Char (toUpper) -- needed for Part 1
+
+{- Part 1
+
+Write a function 'raise' that converts a string to uppercase
+
+Function 'toUpper :: Char -> Char' converts a character to uppercase
+if it is lowercase. All other characters are unchanged
+
+-}
+raise str = map toUpper str
+--raise _ = error "raise not yet implemented"
+
+{- Part 2
+
+Write a function 'nth' that returns the nth element of a list
+
+-}
+nth :: Int -> [a] -> a
+nth n list =list!!(n-1)
+--nth _ _ = error "raise not yet implemented"
+
+{- Part 3
+
+write a function commonLen that compares two sequences
+and reports the length of the prefix they have in common.
+
+-}
+commonLen :: Eq a => [a] -> [a] -> Int
+commonLen (x:xs) (y:ys)
+	| x==y = 1 + commonLen xs ys
+commonLen _ _ = 0
+
---
+++ Ex03.hs
@@ -0,0 +1,50 @@
+{- butrfeld Andrew Butterfield -}
+module Ex03 where
+import Data.List ((\\))
+
+-- Datatypes -------------------------------------------------------------------
+
+-- do not change anything in this section !
+
+-- Binary Tree
+data BT a b
+  = Leaf
+  | Branch (BT a b) a b (BT a b)
+  deriving (Eq, Show)
+
+-- association list
+type Assoc a b = [(a,b)]
+
+-- lookup binary (search) tree
+lkpBST :: Ord a1 => BT a1 a -> a1 -> Maybe a
+lkpBST Leaf _  =  Nothing
+lkpBST (Branch left k d right) k'
+ | k < k'     =  lkpBST left k'
+ | k > k'     =  lkpBST right k'
+ | otherwise  =  Just d
+
+-- Coding Part 1 (13 Marks)
+
+-- insert into binary (search) tree
+insBST :: Ord a => a -> b -> BT a b -> BT a b
+insBST  a b Leaf = Branch Leaf a b Leaf
+insBST a b (Branch leftSub x y rightSub)
+	| a<x 	= Branch(insBST a b leftSub) x y rightSub
+	| a>x	= Branch leftSub x y (insBST a b rightSub)
+	|otherwise = Branch leftSub a b rightSub
+
+-- Coding Part 2 (6 Marks)
+
+-- convert an association list to a binary search tree
+assoc2bst :: Ord a => Assoc a b -> BT a b
+assoc2bst [] = Leaf
+assoc2bst [(x,y)] = insBST x y Leaf
+assoc2bst((x,y):xs)= insBST x y (assoc2bst xs)
+
+-- Coding Part 3 (6 Marks)
+
+-- convert a binary search tree into an (ordered) association list
+bst2assoc :: Ord c =>  BT c e -> Assoc c e
+bst2assoc Leaf =[]
+bst2assoc (Branch Leaf a b Leaf) = [(a,b)]
+bst2assoc (Branch leftSub a b rightSub) = concat[bst2assoc leftSub, [(a,b)], bst2assoc rightSub]
